10da1eb8a4d1290eef4a32244b35607a660e5de8
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-08-02 19:24:22 -0400

    New Angular init class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    styleUrls: ['./custom.css']
+  // styles: ['h1 { font-weight: bold; color: red}',
+  // 'h2 { font-weight: normal; color: blue}']
 })
 export class AppComponent {
-  title = 'hello-world';
+  colors = ["blue", "red", 'green']
 }
